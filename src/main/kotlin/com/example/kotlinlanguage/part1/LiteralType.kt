643b5def5a0275f94e9cb619529314aba9a902f1
sjparkk <sjparkk0820@naver.com>
2023-02-14 22:12:02 +0900

    - part1 : 004 리터럴의 타입

---
+++ src/main/kotlin/com/example/kotlinlanguage/part1/LiteralType.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinlanguage.part1
+
+/**
+ * 리터럴에도 타입이 있고, 표현식의 결과 값도 타입이 있음을 이해
+ *
+ * @param args
+ */
+fun main(args: Array<String>) {
+
+    //정수를 적으면 그 리터럴은 자동으로 Int 타입
+    //그렇기에 variable 뒤에는 :Int 가 생략되어 있음
+    val variable = 10 + 12 - 5
+    println(variable)
+
+}
