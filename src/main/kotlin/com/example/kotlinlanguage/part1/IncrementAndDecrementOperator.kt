bce88a8203dab6b6c24f5e47bd45f357c81d29a7
sjparkk <sjparkk0820@naver.com>
2023-02-17 00:49:14 +0900

    - part1 : 006 증감 연산자

---
+++ src/main/kotlin/com/example/kotlinlanguage/part1/IncrementAndDecrementOperator.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinlanguage.part1
+
+/**
+ * 코틀린의 증감 연산자에 대해 배운다.
+ *
+ */
+fun main(args: Array<String>) {
+
+    // 코틀린의 증감 연산자는 다른 언너의 증감 연산자와 비슷
+    var a = 10
+    var b = 5
+
+    //a++ 은 후위 연산이므로 10 + 5 가 되어 15
+    println(a++ + b)
+
+    //a++ 영향으로 11
+    println(a)
+
+    //전위 감소이므로 5-1 되어 4
+    println(--b)
+
+}
