77cf32d9418fea6c2ea95b5b614d8caf77ae79d4
sjparkk <sjparkk0820@naver.com>
2023-03-29 23:34:55 +0900

    - part1_3 : 042 패키지(Package)

---
+++ src/main/kotlin/com/example/kotlinlanguage/ex_package/EX_Package.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinlanguage.ex_package
+
+class EX_Package {
+
+    fun main(args: Array<String>) {
+        val a = 20
+        val b = -30
+
+        println(max(a, b))
+    }
+
+}
