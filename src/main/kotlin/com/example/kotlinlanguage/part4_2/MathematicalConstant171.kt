0b2c231a44363b439d9ea0d3752c72e64ef5ac5e
sjparkk <sjparkk0820@naver.com>
2023-10-26 22:21:20 +0900

    - part4_2 - 171 E, PI: 수학 상수

---
+++ src/main/kotlin/com/example/kotlinlanguage/part4_2/MathematicalConstant171.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinlanguage.part4_2
+
+import kotlin.math.E
+import kotlin.math.PI
+
+/**
+ * E, PI: 수학 상수
+ *
+ * 코틀린에 내장된 수학 상수에 대해 알아본다.
+ *
+ * kotlin.math 패키지에는 다음과 같은 수학 상수가 선언되어 있다.
+ * const val E: Double
+ * const val PI: Double
+ */
+fun main(args: Array<String>) {
+
+    //원주율 3.141592653589793가 출력
+    println(PI)
+
+    //자연상수 값 2.718281828459045이 출력
+    println(E)
+}
